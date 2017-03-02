e0bf134a54d7f845ec53a53a58894bc151987b81
Cyberia2045 <sagansociety86@Matthews-MacBook-Pro.local>
2017-03-02 12:10:43 -0500

    Added Devise functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 root to: "home#index"
+
+resources :users
+
+devise_for :views
+get "user/new"
+
+devise_for :users, :controllers => { registrations: "registrations" }
+
+get "session/new"
+
 end
