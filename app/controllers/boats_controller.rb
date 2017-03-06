e4ac58af09ed916f34aecdff0cb4cf7fadb432f4
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-06 14:19:37 -0500

    minor changes to format

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,36 +1,52 @@
 class BoatsController < ApplicationController
-  def new
-@boat = Boat.new
-  end
+def new
+    @boat = Boat.new
+ end
 
 
- def create
-	   boat = Boat.new
-	   boat.name = boat_params[:name]
-	  if boat.save
-	  	redirect_to '/'
-	  else
+def show
+    @boat = Boat.find(params[:id])
+ end
 
-	  end
+def create
+       @boat = current_user.boats.new(boat_params)
+    
+         if @boat.save
+             flash[:boat_created] = "New boat was successfully logged."
+             redirect_to '/'
+         end
+end
+
+    def destroy
+        Boat.find(params[:id]).destroy
+        flash[:boat_deleted] = "Boat was successfully deleted"
+        redirect_to root_url
+         
+     end
 
 
-	  
-	end
+     def edit
+        @boat = Boat.find(params[:id])
+     end
+
+    def update
+        @boat = Boat.find(params[:id])
+        if @boat.update_attributes(boat_params)
+          flash[:boat_updated] = "Boat was successfully updated."
+          redirect_to @boat
+        else
+          render 'edit'
+        end
+     end
 
 
 
 
 
 private
-	    
+        
 
-	    def boat_params
-	      params.require(:boat).permit( :name)
-	    end
-
-
-
-
-
-
+        def boat_params
+          params.require(:boat).permit( :name, :total_cont, :avail_cont)
+        end
 end
